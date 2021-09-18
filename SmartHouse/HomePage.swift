14a31be66cacd1d9ba2f9d13bbf84014128735f7
Dingyi <dkang@okstate.edu>
2021-09-18 15:02:10 -0500

    Initial Commit

---
+++ SmartHouse/HomePage.swift
@@ -0,0 +1,19 @@
+//
+//  HomePage.swift
+//  SmartHouse
+//
+//  Created by OSU App Center on 9/18/21.
+//
+
+import UIKit
+
+class HomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
